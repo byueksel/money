31321abeb1b58d20642a070868821ce7e7fb7b1f
Burak Yüksel <brkyksl58@gmail.com>
2017-12-20 17:56:43 +0100

    Initial Checkin

---
+++ src/Byueksel/Money.php
@@ -0,0 +1,198 @@
+<?php
+
+namespace Byueksel;
+
+/**
+ * A simple Money Class for PHP
+ *
+ * Date: 17.12.2017
+ * Time: 01:04
+ *
+ * @package Byueksel
+ * @subpackage Money
+ * @author Burak Yueksel <brkyksl58@gmail.com>
+ * @copyright 2017 Burak Yueksel
+ * @license MIT
+ * @version 1.0.0
+ */
+class Money implements \JsonSerializable
+{
+    /**
+     * @var int
+     */
+    private $amount;
+
+    /**
+     * @var int
+     */
+    private $baseUnit;
+
+    /**
+     * @var string
+     */
+    private $currency;
+
+    /**
+     * @var string
+     */
+    private $locale;
+
+    /**
+     * Retruns the calculated amount
+     * @return float Calculated amount
+     */
+    private function calculateAmount() : float
+    {
+        return $this->amount / $this->baseUnit;
+    }
+
+    /**
+     * Currency constructor.
+     * @param int|float $amount Amount
+     * @param string $currency Currency-Name
+     * @param int $baseUnit Base-Unit
+     */
+    public function __construct($amount = 0, string $currency = 'EUR',
+                                int $baseUnit = 100)
+    {
+        $this->amount = $amount * $baseUnit;
+        $this->baseUnit = $baseUnit;
+        $this->currency = $currency;
+    }
+
+    /**
+     * Returns the Raw Amount
+     * @return int
+     */
+    public function getRawAmount(): int
+    {
+        return $this->amount;
+    }
+
+    /**
+     * Returns the Amount divided by the Base-Unit
+     * @var int $decimals Decimals
+     * @return int|float
+     */
+    public function getAmount(int $decimals = 2): float
+    {
+        $amount = $this->calculateAmount();
+        if($pos = strpos($amount, '.')) {
+            return substr($amount, 0, $pos + 1 + $decimals);
+        }
+
+        return $amount;
+    }
+
+    public function getFormatedAmount($decimals = 2, $decPoint = ',',
+                                      $thousandsSep = '.') :string
+    {
+        $amount = $this->calculateAmount();
+        return number_format($amount, $decimals, $decPoint, $thousandsSep);
+    }
+
+    /**
+     * Returns the Base-Unit
+     * @return int
+     */
+    public function getBaseUnit(): int
+    {
+        return $this->baseUnit;
+    }
+
+    /**
+     * Returns the Currency
+     * @return string
+     */
+    public function getCurrency(): string
+    {
+        return $this->currency;
+    }
+
+    /**
+     * Returns the locale used for LC_MONETARY
+     * @return string
+     */
+    public function getLocale(): string
+    {
+        return $this->locale;
+    }
+
+    /**
+     * Addition
+     * @param Money $money Money-Object
+     * @return $this
+     */
+    public function add(Money $money)
+    {
+        $this->amount = $this->amount + $money->getRawAmount();
+
+        return $this;
+    }
+
+    /**
+     * Subtraction
+     * @param Money $money
+     * @return $this
+     */
+    public function subtract(Money $money)
+    {
+        $this->amount = $this->amount - $money->getRawAmount();
+
+        return $this;
+    }
+
+    /**
+     * @param int|float $multiplier
+     * @return $this
+     */
+    public function multiply($multiplier)
+    {
+        $this->amount = $this->amount * $multiplier;
+
+        return $this;
+    }
+
+    /**
+     * @param  int|float $divisor
+     * @return $this
+     */
+    public function divide($divisor)
+    {
+        $this->amount = $this->amount / $divisor;
+
+        return $this;
+    }
+
+    /**
+     * __toString
+     * @return string
+     */
+    public function __toString()
+    {
+        return (string) $this->getFormatedAmount();
+    }
+
+    /**
+     * __clone
+     * @return Money
+     */
+    public function __clone()
+    {
+        return new Money($this->getRawAmount(), $this->currency, $this->baseUnit);
+    }
+
+    /**
+     * Return Array
+     * @link http://php.net/manual/en/jsonserializable.jsonserialize.php
+     * @return array|mixed data which can be serialized by <b>json_encode</b>
+     */
+    public function jsonSerialize()
+    {
+        return [
+            'amount' => $this->amount,
+            'baseUnit' => $this->baseUnit,
+            'currency' => $this->currency,
+        ];
+    }
+}
---
+++ tests/MoneyTest.php
@@ -0,0 +1,93 @@
+<?php
+
+namespace Byueksel\Tests;
+
+use Byueksel\Money;
+use PHPUnit\Framework\TestCase;
+/**
+ * A simple Money Class for PHP
+ *
+ * Date: 17.12.2017
+ * Time: 01:04
+ *
+ * @package Byueksel
+ * @subpackage Test
+ * @author Burak Yueksel <brkyksl58@gmail.com>
+ * @copyright 2017 Burak Yueksel
+ * @license MIT
+ * @version 1.0.0
+ */
+class MoneyTest extends TestCase
+{
+
+    public function testCurrencyCalculations()
+    {
+        $mon1 = new Money(100.73);
+        $mon1->add(new Money(99.27));
+        $this->assertEquals(200, $mon1->getAmount());
+
+        $mon2 = new Money(100.74);
+        $mon2->add(new Money(99.27));
+        $this->assertEquals(200.01, $mon2->getAmount());
+
+        $mon3 = new Money(0.99);
+        $mon3->add(new Money(1.66));
+        $this->assertEquals(2.65, $mon3->getAmount());
+
+        $mon4 = new Money(0.99);
+        $mon4->multiply(4.66);
+        $this->assertEquals(4.6134, $mon4->getAmount(4));
+    }
+
+    public function testCurrencyOneEuro()
+    {
+        $cur = new Money(1);
+
+        $this->assertEquals(1, $cur->getAmount());
+    }
+
+    public function testCurrencyOneEuroThirtyCent()
+    {
+        $cur = new Money(1.30);
+
+        $this->assertEquals(1.30, $cur->getAmount());
+    }
+
+    public function testCurrencyAddition()
+    {
+        $total = new Money();
+        $total->add(new Money(14670.40));
+        $total->add(new Money(2787.37));
+        $total->add(new Money(49.00));
+
+        $this->assertEquals(17506.77, $total->getAmount());
+    }
+
+    public function testCurrencyAdditionFormated()
+    {
+        $total = new Money();
+        $total->add(new Money(14670.40));
+        $total->add(new Money(2787.37));
+        $total->add(new Money(49.00));
+
+        $this->assertEquals('17.506,77',
+            $total->getFormatedAmount(2));
+    }
+
+    public function testCurrencyMultiply()
+    {
+        $money = new Money(84);
+        $money->multiply(1.19);
+
+        $this->assertEquals(99.96, $money->getAmount());
+    }
+
+    public function testCurrencyDivide()
+    {
+        $money = new Money(84);
+        $money->divide(1.19);
+
+        $this->assertEquals(70.58, $money->getAmount());
+    }
+
+}
